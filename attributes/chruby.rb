67e8fcb9e8fd872cc766495868f298a865b2dcca
Igor Kurochkin <ikurochkin@express42.com>
2014-12-04 14:49:50 +0300

    Fix reference

--- attributes/chruby.rb
+++ attributes/chruby.rb
@@ -1,6 +1,5 @@
 default['ruby']['chruby']['install_path'] = '/opt/chruby'
 default['ruby']['chruby']['home_prefix'] = '/home'
 default['ruby']['chruby']['git_url'] = 'git://github.com/postmodern/chruby.git'
-# You should fix version of chruby
-# default['ruby']['chruby']['git_ref'] = "master"
+default['ruby']['chruby']['git_ref'] = 'master'
 default['ruby']['chruby']['install_pkgs'] =  %w(make git-core)
--- attributes/ruby_build.rb
+++ attributes/ruby_build.rb
@@ -1,5 +1,4 @@
 default['ruby']['ruby_build']['install_path'] = '/opt/ruby-build'
 default['ruby']['ruby_build']['git_url'] = 'git://github.com/sstephenson/ruby-build.git'
-# You should fix ruby-build version
-# default['ruby']['ruby_build']['git_ref'] = "master"
+default['ruby']['ruby_build']['git_ref'] = 'master'
 default['ruby']['ruby_build']['install_pkgs'] =  %w(build-essential bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev ssl-cert subversion git-core)
