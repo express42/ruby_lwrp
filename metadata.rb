aa61b3ed95f9d9d28d9a875cb5157972702db5d0
Sergey Bogatyrets <serj.sj@gmail.com>
2015-01-05 22:11:13 +0300

    More dev packages for successful build ruby 2.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@express42.com'
 license          'MIT'
 description      'Installs chruby and ruby-build to build rubies from source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 recipe           'ruby::default', 'Includes `ruby_build` and `chruby` recipes.'
 recipe           'ruby::chruby', 'Installs the Chruby to `/opt/chruby` and prepares Chef to `ruby_set` the LWRP.'
