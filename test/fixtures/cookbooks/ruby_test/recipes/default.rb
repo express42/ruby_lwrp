6e4ce2cab2bd08ddf795bb1e606844f2173a3ad8
Nikita Borzykh <sample.n@gmail.com>
2015-03-05 14:41:28 +0300

    Use test_user instead vagrant

--- test/fixtures/cookbooks/ruby_test/recipes/default.rb
+++ test/fixtures/cookbooks/ruby_test/recipes/default.rb
@@ -1,11 +1,16 @@
 include_recipe 'apt'
 include_recipe 'ruby_lwrp'
 
+user 'test_user' do
+  home '/home/test_user'
+  supports manage_home: true
+end
+
 ruby_install '2.1.5' do
   action :install
 end
 
 ruby_set '2.1.5' do
   action :set
-  username 'vagrant'
+  username 'test_user'
 end
