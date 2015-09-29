3395def285f0096635478be1ca69148d3e8d94d4
Nikita Borzykh <sample.n@gmail.com>
2015-09-29 16:31:26 +0300

    Rubocop fixes

--- attributes/chruby.rb
+++ attributes/chruby.rb
@@ -2,4 +2,4 @@ default['ruby']['chruby']['install_path'] = '/opt/chruby'
 default['ruby']['chruby']['home_prefix'] = '/home'
 default['ruby']['chruby']['git_url'] = 'git://github.com/postmodern/chruby.git'
 default['ruby']['chruby']['git_ref'] = 'master'
-default['ruby']['chruby']['install_pkgs'] =  %w(make git-core)
+default['ruby']['chruby']['install_pkgs'] = %w(make git-core)
